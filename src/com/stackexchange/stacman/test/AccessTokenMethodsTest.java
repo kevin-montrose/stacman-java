6273431607ab603bb0e72d5ee35377a7e97f72f8
kevin-montrose <kevin.montrose@gmail.com>
2013-02-19 13:24:06 -0500

    tests

---
+++ src/com/stackexchange/stacman/test/AccessTokenMethodsTest.java
@@ -0,0 +1,10 @@
+package com.stackexchange.stacman.test;
+
+import org.junit.Test;
+
+public class AccessTokenMethodsTest {
+    @Test
+    public void testInvalidate() throws Exception {
+
+    }
+}
